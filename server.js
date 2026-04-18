52a04946ac939d66492682d57306e78ab21302ad
jrmbjr <jrmbjr@gmail.com>
2026-04-18 17:50:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -180,6 +180,8 @@ app.post("/query", autenticar, async (req, res) => {
 // ==============================
 const PORT = process.env.PORT || 3000;
 
+console.log("PORT ENV:", process.env.PORT);
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta " + PORT);
 });
