2a908c1ee838be233ce42e7fe58b9bc6e09c7338
jrmbjr <jrmbjr@gmail.com>
2026-04-18 18:05:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/ping", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando na porta " + PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Servidor rodando na porta " + process.env.PORT);
 });
