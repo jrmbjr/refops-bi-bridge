4efc484af0c1d309096eb05f83c87812852d9b4d
jrmbjr <jrmbjr@gmail.com>
2026-04-18 13:41:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("rodando na porta", PORT);
 });
+
+app.get("/", (req, res) => {
+  res.send("RefOps BI Bridge rodando");
+});
