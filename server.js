144286e33bb88f881679d2163bc99cf90d69c8ab
jrmbjr <jrmbjr@gmail.com>
2026-04-18 16:46:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -168,7 +168,11 @@ app.post("/query", autenticar, async (req, res) => {
 // ==============================
 // START SERVER
 // ==============================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  throw new Error("PORT não definida pelo Railway");
+}
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta " + PORT);
